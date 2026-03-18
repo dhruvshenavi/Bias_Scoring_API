250df49f0ee58d53d031861ae1d5dba2d2e18a88
22110022-blip <22110022@pvgcoet.ac.in>
2026-03-18 13:03:15 +0530

    Check for GPU and show In terminal

--- app/services/model_loader.py
+++ app/services/model_loader.py
@@ -12,8 +12,17 @@ def get_pipeline():
     if _zs_pipeline is not None:
         return _zs_pipeline
 
-    device = 0 if torch.cuda.is_available() else -1
-    torch_dtype = torch.float16 if torch.cuda.is_available() else None
+    use_gpu = torch.cuda.is_available()
+
+    if use_gpu:
+        print(torch.cuda.get_device_name(0))
+
+    device = 0 if use_gpu else -1
+    torch_dtype = torch.float16 if use_gpu else None
+
+    print("Using GPU:", torch.cuda.is_available())
+    print("Device:", device)
+
 
     _zs_pipeline = pipeline(
         "zero-shot-classification",
